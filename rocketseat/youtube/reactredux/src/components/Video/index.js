93b8e86fb8a3bc5b3ed098f891999db8616cea41
Diego Santos <dsaantos1995@gmail.com>
2020-01-20 20:11:55 -0300

    updated

---
+++ rocketseat/youtube/reactredux/src/components/Video/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const Video = ({ activeModule, activeLesson}) => (
+  <div>
+    <strong>Modulo {activeModule.title}</strong>
+    <br />
+    <span>Aula {activeLesson.title}</span>
+  </div>  
+);
+
+export default connect(state => ({
+  activeLesson: state.course.activeLesson,
+  activeModule: state.course.activeModule
+}))(Video);
