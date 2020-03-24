2e10026b47aa0fdb541d1311bfc6488bbc406a53
Diego Santos <dsaantos1995@gmail.com>
2020-03-24 19:59:48 -0300

    remove some paths

--- react-testing/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Main from './Main';
-import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<Main title="Titulo da pagina" description="Aqui um breve resumo de test" />, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
