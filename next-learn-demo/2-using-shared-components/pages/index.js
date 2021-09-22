68d44cfe13d9b57ca812c81f09e6ec942824d18f
Diego Santos <dsaantos1995@gmail.com>
2021-09-22 13:40:46 -0300

    updating path

--- next-learn-demo/2-using-shared-components/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Layout from './components/MyLayout';
-
-const indexPageContent = <p>Hello Next.js</p>;
-
-export default function Index() {
-  return <Layout content={indexPageContent} />;
-}
--- next-learn-demo/readme.md
+++
@@ -1,3 +0,0 @@
-# nextjs.org/learn demo content
-
-This repository is meant to be used with the Next.js tutorial on https://nextjs.org/learn.
